f96fd67994891b25eef394f4c05676ebce253e9c
glucaci <gabriel.lucaci.dev@gmail.com>
2018-11-09 11:41:37 +0100

    Add sample console app

---
+++ src/GenericHost.Sample/Program.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Thor.GenericHost;
+
+namespace GenericHost.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> configuration = new Dictionary<string, string>
+            {
+                {"Tracing:ApplicationId", "999"},
+                {"Tracing:Level", "Verbose"},
+                {"Tracing:BlobStorage:ConnectionString", "***"},
+                {"Tracing:BlobStorage:AttachmentContainerName", "demo"},
+                {"Tracing:EventHub:ConnectionString", "***"}
+            };
+
+            IHost host = new HostBuilder()
+                .ConfigureHostConfiguration(builder =>
+                    builder.AddInMemoryCollection(configuration))
+                .ConfigureServices(services =>
+                    services.AddHostedService<MyService>())
+                .BuildWithTracing();
+
+            host.Run();
+        }
+    }
+}
